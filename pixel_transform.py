475c2ad9e333fbf430a6e3ea1096f4f28ddfb6a3
Elizabeth Chilcoat <elizabeth@Elizabeths-MacBook-Pro.local>
2020-09-22 18:15:39 -0400

    Added save and prompt for bitplane

--- pixel_transform.py
+++ pixel_transform.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, filedialog, Frame, Label, Button
+from tkinter import Tk, filedialog, Frame, Label, Button, simpledialog, filedialog
 from PIL import Image, ImageTk
 import cv2
 import numpy as np
@@ -51,91 +51,41 @@ def select_image(event):
 def neg_img(event):
     global image
     neg_img = 255-image
-    print("HI")
     update_new(neg_img)
 
 def bitplane(color, bit):
     global image
+    
+    # Faster numpy trick
     bitplane_img = np.zeros((image.shape[0], image.shape[1], 3), dtype=np.uint8)
-
-    for i in range(image.shape[0]):
+    bitplane_img[:,:,color][image[:,:,color]% 2**(bit+1) >= 2**bit] = np.uint8(255)
+    
+    """for i in range(image.shape[0]):
         for j in range(image.shape[1]):
             if image[i][j][color] % 2**(bit+1) >= 2**bit:
-                bitplane_img[i][j][color] = 255
-
+                bitplane_img[i][j][color] = 255"""
+                
     update_new(bitplane_img)
+    
+    
+def prompt_bitplane(event):
+    colors = ["blue", "green", "red"]
+    while(True):
+        color = simpledialog.askstring("Input", "What color? (red, green, or blue)",
+                                       parent=root)
+        if color.lower() in colors:
+            color_code = colors.index(color)
+            break
 
-def bitplane_blue_0(event):
-    bitplane(0,0)
-    
-def bitplane_blue_1(event):
-    bitplane(0,1)
-    
-def bitplane_blue_2(event):
-    bitplane(0,2)
+    while (True):
+        bit = simpledialog.askinteger("Input", "What bit value? (0-7)",
+                                         parent=root,
+                                         minvalue=0, maxvalue=7)
+        if bit != None:
+            break
+        
+    bitplane(color_code, bit)
 
-def bitplane_blue_3(event):
-    bitplane(0,3)
-
-def bitplane_blue_4(event):
-    bitplane(0,4)
-    
-def bitplane_blue_5(event):
-    bitplane(0,5)
-    
-def bitplane_blue_6(event):
-    bitplane(0,6)
-    
-def bitplane_blue_7(event):
-    bitplane(0,7)
-    
-def bitplane_green_0(event):
-    bitplane(1,0)
-    
-def bitplane_green_1(event):
-    bitplane(1,1)
-    
-def bitplane_green_2(event):
-    bitplane(1,2)
-    
-def bitplane_green_3(event):
-    bitplane(1,3)
-    
-def bitplane_green_4(event):
-    bitplane(1,4)
-    
-def bitplane_green_5(event):
-    bitplane(1,5)
-    
-def bitplane_green_6(event):
-    bitplane(1,6)
-    
-def bitplane_green_7(event):
-    bitplane(1,7)
-    
-def bitplane_red_0(event):
-    bitplane(2,0)
-    
-def bitplane_red_1(event):
-    bitplane(2,1)
-    
-def bitplane_red_2(event):
-    bitplane(2,2)
-    
-def bitplane_red_3(event):
-    bitplane(2,3)
-    
-def bitplane_red_4(event):
-    bitplane(2,4)
-    
-def bitplane_red_5(event):
-    bitplane(2,5)
-    
-def bitplane_red_6(event):
-    bitplane(2,6)
-    
-def bitplane_red_7(event):
-    bitplane(2,7)
     
   
 def update_original(path):
@@ -148,13 +98,16 @@ def update_original(path):
     return disp_img
 
 def update_new(img):    
-    global new
+    global new, new_img
+    new_img = img
     disp_img = convert_img(img)
     new.configure(image=disp_img)
     new.image = disp_img
-
     
-
+def save(event):
+    name = filedialog.asksaveasfilename(confirmoverwrite=True)
+    cv2.imwrite(name, new_img)
+    
     
 
 def main():
@@ -174,7 +127,7 @@ def main():
     frame = Frame()
     frame.pack()
     
-        # button for select image
+    # button for negative image
     btn_neg = Button(
         master = frame,
         text = "Negative",
@@ -182,175 +135,16 @@ def main():
     )
     btn_neg.grid(row = 0, column = 0)
     btn_neg.bind('<ButtonRelease-1>', neg_img)
-
-    btn_bitplane_blue_7 = Button(
-        master=frame,
-        text="Blue 7"
-    )
-    btn_bitplane_blue_7.grid(row=1, column=0)
-    btn_bitplane_blue_7.bind('<ButtonRelease-1>', bitplane_blue_7)
-
-    btn_bitplane_blue_6 = Button(
-        master=frame,
-        text="Blue 6"
-    )
-    btn_bitplane_blue_6.grid(row=1, column=1)
-    btn_bitplane_blue_6.bind('<ButtonRelease-1>', bitplane_blue_6)
-
-    btn_bitplane_blue_5 = Button(
-        master=frame,
-        text="Blue 5"
-    )
-    btn_bitplane_blue_5.grid(row=1, column=2)
-    btn_bitplane_blue_5.bind('<ButtonRelease-1>', bitplane_blue_5)
-
-    btn_bitplane_blue_4 = Button(
-        master=frame,
-        text="Blue 4"
-    )
-    btn_bitplane_blue_4.grid(row=1, column=3)
-    btn_bitplane_blue_4.bind('<ButtonRelease-1>', bitplane_blue_4)
-
-    btn_bitplane_blue_3 = Button(
-        master=frame,
-        text="Blue 3"
-    )
-    btn_bitplane_blue_3.grid(row=1, column=4)
-    btn_bitplane_blue_3.bind('<ButtonRelease-1>', bitplane_blue_3)
-
-    btn_bitplane_blue_2 = Button(
-        master=frame,
-        text="Blue 2"
-    )
-    btn_bitplane_blue_2.grid(row=1, column=5)
-    btn_bitplane_blue_2.bind('<ButtonRelease-1>', bitplane_blue_2)
-
-    btn_bitplane_blue_1 = Button(
-        master=frame,
-        text="Blue 1"
-    )
-    btn_bitplane_blue_1.grid(row=1, column=6)
-    btn_bitplane_blue_1.bind('<ButtonRelease-1>', bitplane_blue_1)
-
-    btn_bitplane_blue_0 = Button(
-        master=frame,
-        text="Blue 0"
-    )
-    btn_bitplane_blue_0.grid(row=1, column=7)
-    btn_bitplane_blue_0.bind('<ButtonRelease-1>', bitplane_blue_0)
-
-    btn_bitplane_green_7 = Button(
-        master=frame,
-        text="Green 7"
-    )
-    btn_bitplane_green_7.grid(row=2, column=0)
-    btn_bitplane_green_7.bind('<ButtonRelease-1>', bitplane_green_7)
-
-    btn_bitplane_green_6 = Button(
-        master=frame,
-        text="Green 6"
-    )
-    btn_bitplane_green_6.grid(row=2, column=1)
-    btn_bitplane_green_6.bind('<ButtonRelease-1>', bitplane_green_6)
-
-    btn_bitplane_green_5 = Button(
-        master=frame,
-        text="Green 5"
-    )
-    btn_bitplane_green_5.grid(row=2, column=2)
-    btn_bitplane_green_5.bind('<ButtonRelease-1>', bitplane_green_5)
-
-    btn_bitplane_green_4 = Button(
-        master=frame,
-        text="Green 4"
-    )
-    btn_bitplane_green_4.grid(row=2, column=3)
-    btn_bitplane_green_4.bind('<ButtonRelease-1>', bitplane_green_4)
-
-    btn_bitplane_green_3 = Button(
-        master=frame,
-        text="Green 3"
-    )
-    btn_bitplane_green_3.grid(row=2, column=4)
-    btn_bitplane_green_3.bind('<ButtonRelease-1>', bitplane_green_3)
-
-    btn_bitplane_green_2 = Button(
-        master=frame,
-        text="Green 2"
-    )
-    btn_bitplane_green_2.grid(row=2, column=5)
-    btn_bitplane_green_2.bind('<ButtonRelease-1>', bitplane_green_2)
-
-    btn_bitplane_green_1 = Button(
-        master=frame,
-        text="Green 1"
-    )
-    btn_bitplane_green_1.grid(row=2, column=6)
-    btn_bitplane_green_1.bind('<ButtonRelease-1>', bitplane_green_1)
-
-    btn_bitplane_green_0 = Button(
-        master=frame,
-        text="Green 0"
-    )
-    btn_bitplane_green_0.grid(row=2, column=7)
-    btn_bitplane_green_0.bind('<ButtonRelease-1>', bitplane_green_0)
-
-    btn_bitplane_red_7 = Button(
-        master=frame,
-        text="Red 7"
-    )
-    btn_bitplane_red_7.grid(row=3, column=0)
-    btn_bitplane_red_7.bind('<ButtonRelease-1>', bitplane_red_7)
-
-    btn_bitplane_red_6 = Button(
-        master=frame,
-        text="Red 6"
-    )
-    btn_bitplane_red_6.grid(row=3, column=1)
-    btn_bitplane_red_6.bind('<ButtonRelease-1>', bitplane_red_6)
-
-    btn_bitplane_red_5 = Button(
-        master=frame,
-        text="Red 5"
-    )
-    btn_bitplane_red_5.grid(row=3, column=2)
-    btn_bitplane_red_5.bind('<ButtonRelease-1>', bitplane_red_5)
-
-    btn_bitplane_red_4 = Button(
-        master=frame,
-        text="Red 4"
-    )
-    btn_bitplane_red_4.grid(row=3, column=3)
-    btn_bitplane_red_4.bind('<ButtonRelease-1>', bitplane_red_4)
-
-    btn_bitplane_red_3 = Button(
-        master=frame,
-        text="Red 3"
-    )
-    btn_bitplane_red_3.grid(row=3, column=4)
-    btn_bitplane_red_3.bind('<ButtonRelease-1>', bitplane_red_3)
-
-    btn_bitplane_red_2 = Button(
-        master=frame,
-        text="Red 2"
-    )
-    btn_bitplane_red_2.grid(row=3, column=5)
-    btn_bitplane_red_2.bind('<ButtonRelease-1>', bitplane_red_2)
-
-    btn_bitplane_red_1 = Button(
-        master=frame,
-        text="Red 1"
-    )
-    btn_bitplane_red_1.grid(row=3, column=6)
-    btn_bitplane_red_1.bind('<ButtonRelease-1>', bitplane_red_1)
-
-    btn_bitplane_red_0 = Button(
-        master=frame,
-        text="Red 0"
-    )
-    btn_bitplane_red_0.grid(row=3, column=7)
-    btn_bitplane_red_0.bind('<ButtonRelease-1>', bitplane_red_0)
     
+    # button for bitpiece
+    btn_bit = Button(
+        master = frame,
+        text = "Bitplane",
+        underline = 0
+    )
+    btn_bit.grid(row = 0, column = 1)
+    btn_bit.bind('<ButtonRelease-1>', prompt_bitplane)
+
     
     # button for select image
     btn_select = Button(
@@ -358,8 +152,17 @@ def main():
         text = "Select an Image",
         underline = 0
     )
-    btn_select.grid(row = 4, column = 0)
+    btn_select.grid(row = 1, column = 1)
     btn_select.bind('<ButtonRelease-1>', select_image)
+    
+    # button for select image
+    btn_save = Button(
+        master = frame,
+        text = "Save Image",
+        underline = 0
+    )
+    btn_save.grid(row = 1, column = 0)
+    btn_save.bind('<ButtonRelease-1>', save)
     
 
     # Bind all the required keys to functions
