ceb47a39005708c08c75d791fd718205cf042941
kelle clark <clarkke20@students.ecu.edu>
2020-09-23 20:16:26 -0400

    going with EC's version

--- logBranch.py
+++ logBranch.py
@@ -26,7 +26,27 @@ def opencv_img(path):
     # read and convert image
     image = cv2.imread(path)
     img = cv2.resize(image, (0,0), fx=0.5, fy=0.5) 
-    return(img)
+    defaultrows = 420
+    defaultcolumn = 580
+    # Set scale multiplier to the lowest of the following values:
+    # 1
+    # window row count / image row count
+    # window column count / image column count
+    scale = min(1, min(defaultrows / image.shape[0], defaultcolumn / image.shape[1]))
+
+    # Set triangle corners used for affine transformation to top left, top right, and bottom left corners of image
+    srcTri = np.array([[0, 0], [image.shape[1] - 1, 0], [0, image.shape[0] - 1]]).astype(np.float32)
+
+    # Set location of top right and bottom left corners of resized image
+    dstTri = np.array( [[0, 0], [int(image.shape[1] * scale), 0], [0, int(image.shape[0] * scale)]] ).astype(np.float32)
+
+    # Perform affine transformation to resize image
+    warp_mat = cv2.getAffineTransform(srcTri, dstTri)
+    image = cv2.warpAffine(image, warp_mat, (image.shape[1], image.shape[0]))
+
+    # Trim black border from resized image
+    image = image[0:int(image.shape[0] * scale), 0:int(image.shape[1] * scale)]
+    return(image)
     
     
 # Convert it to ImageTK
@@ -37,6 +57,7 @@ def convert_img(image):
     im = Image.fromarray(image)
     imgtk = ImageTk.PhotoImage(image=im)
     return(imgtk)
+
 ##---------------------------------------------------------------------------##
 
 ##--------Functions to display the metadata of images------------------------##
@@ -134,40 +155,49 @@ def bitplane(color, bit):
 def log_trans(event):
     global image
     
-    log_img = np.zeros((image.shape[0], image.shape[1], 3), dtype=np.uint8)
+    cmax =np.max(image)  
     
-    cmax_red = np.amax(image[:,:,0])
-    cmax_green = np.amax(image[: ,:, 1])
-    cmax_blue = np.amax(image[: ,:,2])
+    cpy_img = image.copy()
     
-    if cmax_red == 255:
-        cred = 255/math.log(255,10)
-    elif cmax_red < 10:
-        cred = 1
-    else:
-        cred = 255/math.log(cmax_red + 1,10)
-        
-    if cmax_green == 255:
-        cgreen = 255/math.log(.01,10)
-    elif cmax_green < 10:
-        cgreen = 1
-    else:
-        cgreen = 255/math.log(cmax_green + 1,10) 
-        
-    if cmax_blue == 255:
-        cblue = 255/math.log(.01,10)
-    elif cmax_blue < 10:
-        cblue = 1
-    else:
-        cblue = 255/math.log(cmax_blue + 1 ,10)
-    
-    for i in range(0, image.shape[0]-1):
-        for j in range(0, image.shape[1]-1):
-            log_img[i,j,0] = np.uint8(255 * math.log(1 + image[i,j,0],10)/math.log(1+ cmax_red,10))
-            log_img[i,j,1] = np.uint8(cgreen * math.log(1 + image[i,j,1],10))
-            log_img[i,j,2] = np.uint8(cblue * math.log(1 + image[i,j,2],10))
-
+    # Add 1 to all pixel values except those at 255 to prevent overflow
+    cpy_img[cpy_img<255] += 1
+    maximum = np.max(cpy_img)
    
+    #Prevent divide by 0 due to overflow
+    if maximum != 255:
+        maximum = maximum + 1
+   
+    #Log transformation
+    log_img = (255/math.log(maximum)) * np.log(cpy_img)
+    
+    # if cmax_red == 255:
+    #     cred = 255/math.log(255,10)
+    # elif cmax_red == 9:
+    #     cred = 1
+    # else:
+    #     cred = 255/math.log(cmax_red + 1,10)
+        
+    # if cmax_green == 255:
+    #     cgreen = 255/math.log(.01,10)
+    # elif cmax_green == 10:
+    #     cgreen = 1
+    # else:
+    #     cgreen = 255/math.log(cmax_green + 1,10) 
+        
+    # if cmax_blue == 255:
+    #     cblue = 255/math.log(.01,10)
+    # elif cmax_blue == 10:
+    #     cblue = 1
+    # else:
+    #     cblue = 255/math.log(cmax_blue + 1 ,10)
+    
+    # for i in range(0, image.shape[0]-1):
+    #     for j in range(0, image.shape[1]-1):
+    #         log_img[i,j,0] = np.uint8(255 * math.log(1 + image[i,j,0],10)/math.log(1+ cmax_red,10))
+    #         log_img[i,j,1] = np.uint8(cgreen * math.log(1 + image[i,j,1],10))
+    #         log_img[i,j,2] = np.uint8(cblue * math.log(1 + image[i,j,2],10))
+
+    log_img = np.array(log_img, dtype = np.uint8)
     update_new(log_img)
         
     
@@ -180,11 +210,11 @@ def main():
     
     # The original loaded image
     original = Label(image=None)
-    original.pack(side="top", padx=10, pady=10)
+    original.pack(side="right", padx=10, pady=10)
     
     # The new modifed image
     new = Label(image=None)
-    new.pack(side="top", padx=10, pady=10)
+    new.pack(side="left", padx=10, pady=10)
 
     # Frame to display navigation buttons at bottom of window
     frame = Frame()
